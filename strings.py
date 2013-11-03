892e57633ba762652efa6bb258f9c0238e1bd930
bzw1377 <bzw1377@rit.edu>
2013-11-03 17:29:49 -0500

    Added a few comments

--- strings.py
+++ strings.py
@@ -1,6 +1,8 @@
 import string
 
+#Function to extract readable ascii strings with greater than four characters from a file
 def strings(filename, min=4):
+	#Open filename (which will be passed as cli arg
     with open(filename, "rb") as f:
         result = ""
         for c in f.read():
@@ -10,5 +12,6 @@ def strings(filename, min=4):
             if len(result) >= min:
                 yield result
             result = ""
-sl = list(strings("pcap_fileName"))
+#Store all the strings found in file in a list. Filename will be from cli arg eventually
+sl = list(strings("fileName"))
 print sl
